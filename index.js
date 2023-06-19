0aaa5d547d379acb3e74f3caeb5ffc133a1033e5
Jek <136816441+GhrifJek@users.noreply.github.com>
2023-06-19 17:13:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let myFavoriteDrink="Капучино";
+let myFavoriteAnimal="Собака";
+let myFavoriteProgrammingLanguage="Java Script";
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
 
